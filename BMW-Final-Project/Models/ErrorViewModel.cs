9b88a79ddcf904f0b1bce5b4cc08e6880f054790
93ToXic93 <122994130+93ToXic93@users.noreply.github.com>
2024-02-23 00:43:58 +0200

    Add project files.

---
+++ BMW-Final-Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BMW_Final_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
