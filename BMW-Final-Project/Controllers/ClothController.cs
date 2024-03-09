dc3c571ec4d5080276479a02009d5bb27ea4598b
93ToXic93 <122994130+93ToXic93@users.noreply.github.com>
2024-03-09 20:07:01 +0200

    made some changes on the views and added cloth controller

---
+++ BMW-Final-Project/Controllers/ClothController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BMW_Final_Project.Controllers
+{
+    public class ClothController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
